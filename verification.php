bbdc87bd69aeeac719b0e936d67bebbac211e4ec
GGive26 <precieudieudonne@gmail>
2023-10-09 10:12:09 -0400

    length and codage

--- verification.php
+++ verification.php
@@ -1,18 +1,40 @@
 <?php
+require_once('widgets.php');
+$cle = $_POST['password'];
+if ($_POST){ 
+    function Encodage($cle){
+        $precieu= addSAlt($cle);
+        echo$precieu;
+        echo"<br";
+        codage($cle);
+    }
 function verification(){
-    require_once('widgets.php');
-    if ($_POST){
-        $cle = $_POST['password'];
+    $cle = $_POST['password'];
     if(empty($cle)){
-        echo"<br>Vous n'avez pas entrer de mot de passe ";}
+        echo"<br>Vous n'avez pas entrer de mot de passe ";
+        $success=false;
+    }
     else{
         echo"<br>Votre mot de passe est : ".$cle;
+        $success=true;
     }
-    PasswordLengthIsValid($cle);
+    echo"<br>";
+    echo"<br>";
+    if($success){
+    $verification1= PasswordLength($cle);
+     if(!$verification1){
+        echo"<br>";
+        echo"<p>Vous pouvez voir ici le codage de votre mot de passe 
+        d'abord avec ajout de sel ensuite avec du sha1 </p>";
+     Encodage($cle);
+     echo"<br>";
+     }
+    }
+    else{ echo"Veuillez retourner sur la page précedente et entrer un mot de passe valide ";
     }
 }
 verification();
-
+}
 ?>
 <br><br>
 <a href="index.php" >Précedent</a>
--- widgets.php
+++ widgets.php
@@ -1,22 +1,27 @@
 <?php
-function PasswordLengthIsValid($PasswordToValid){
+function PasswordLength($Password){
     //le mot de passe comprendra entre 6 et 10 caractère
-    $length = strlen($PasswordToValid);
-    $responses=[
-        'isValid'=>true,
-        'msg'=>''
-    ];
-    if ($length<2){
-        $responses=[
-            'isValid'=>false,
-            'msg'=>'Le mot de passe ne correspond pas aux critères'
-        ];
-    }elseif($length>10){
-        $responses=[
-            'isValid'=>false,
-            'msg'=>'Le mot de passe ne correspond pas aux critères'
-        ];
+    $length = strlen($Password);
+    $responses="";
+    $collect="reussi";
+    if ($length<6){
+       echo $responses='Le mot de passe ne correspond pas aux critères'
+        ;
     }
-return $responses;    
+    elseif($length>10){
+       echo $responses ='Le mot de passe ne correspond pas aux critères';
+    } 
+    return $responses; 
+}
+
+
+function addSAlt($cle){
+    $salt="precieudieudonne";
+    $saltedName=$salt.$cle.$salt;
+    return $saltedName;
+}
+function codage($cle){
+    $EncodName= sha1($cle);
+    return $EncodName;
 }
 ?>
